6132492eb97e9aa782fe539bb5924e30c80bc9ee
primolechuga <juanestebaca123@gmail.com>
2025-02-07 13:49:06 -0500

    refactor: Se cambia pagina de registro

--- westeros-front/src/pages/RegisterPage.tsx
+++ westeros-front/src/pages/RegisterPage.tsx
@@ -1,14 +1,10 @@
 // src/pages/RegisterPage.tsx
 import React from 'react';
 import Register from '../components/register'; // Importar el componente Register
-import LogoAppBar from '../components/logoAppBar'; // Importar el componente LogoAppBar
 
 const RegisterPage: React.FC = () => {
   return (
-    <div>
-      <LogoAppBar /> {/* Barra con solo el logo */}
-      <Register /> {/* Componente de registro */}
-    </div>
+      <Register /> 
   );
 };
 
