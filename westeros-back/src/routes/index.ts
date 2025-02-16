ba7529c8d9eb1f579534487fc7accbbd5f94377d
primolechuga <juanestebaca123@gmail.com>
2025-02-15 19:43:06 -0500

    fix: Se quita de momento el userAuth

--- westeros-back/src/routes/index.ts
+++ westeros-back/src/routes/index.ts
@@ -4,12 +4,11 @@ import { houseRouter } from './house.routes';
 import { productRouter } from './product.routes';
 import { merchantsRouter } from './merchants.routes';
 import { Router } from 'express';
-import { userAuth } from '../middlewares/userAuth';
+// import { userAuth } from '../middlewares/userAuth';
 
 
 const router = Router();
 
-router.use(userAuth);
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
 router.use('/house', houseRouter);
